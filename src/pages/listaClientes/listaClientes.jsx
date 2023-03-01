892ec9ae455d6eacfb7ae545aa02f91e58943315
Emanuele Maria <emanuuuelem@gmail.com>
2023-03-01 17:31:42 -0300

    crate table to list clients

--- src/pages/listaClientes/listaClientes.jsx
+++ src/pages/listaClientes/listaClientes.jsx
@@ -1,4 +1,38 @@
+import HeaderSecond from "../../components/HeaderSecond";
+import './style.scss'
+
 const ListaClientes = () => {
-    return 
+    const clients = []
+    return (
+        <div>
+            <HeaderSecond titulo="Listar clientes" />
+            <section>
+                <table>
+                    <tr>
+                        <th>Código</th><th>Nome</th><th>Idade</th><th>Peso</th><th>Altura</th><th>Nascimento</th><th>Último pagamento</th><th>Ação</th>
+                    </tr>
+                </table>
+                <div className='content'>
+                    {/* {clients?.map((item) => {
+                        return (
+                            <div>
+                                <table>
+                                    <tr>
+                                        <td>aaaaa</td><td>{item.tipo == 'D' ? 'Deposito' : 'Saque'}</td><td>{item.data}</td>
+                                    </tr>
+                                </table>
+                            </div>
+                        )
+                    })} */}
+                    <table>
+                        <tr>
+                            <td>aa</td><td>bb</td>
+                        </tr>
+                    </table>
+
+                </div>
+            </section>
+        </div>
+    )
 }
 export default ListaClientes;
