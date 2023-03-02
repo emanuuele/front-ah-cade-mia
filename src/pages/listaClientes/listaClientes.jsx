703e7253c0fdc8b71ff023431adc2f03f4bb4375
Emanuele Maria <emanuuuelem@gmail.com>
2023-03-01 23:34:45 -0300

    add table of list of clients

--- src/pages/listaClientes/listaClientes.jsx
+++ src/pages/listaClientes/listaClientes.jsx
@@ -1,38 +1,72 @@
+import { Table } from "antd";
 import HeaderSecond from "../../components/HeaderSecond";
-import './style.scss'
+import "./style.scss";
 
 const ListaClientes = () => {
-    const clients = []
-    return (
-        <div>
-            <HeaderSecond titulo="Listar clientes" />
-            <section>
-                <table>
-                    <tr>
-                        <th>Código</th><th>Nome</th><th>Idade</th><th>Peso</th><th>Altura</th><th>Nascimento</th><th>Último pagamento</th><th>Ação</th>
-                    </tr>
-                </table>
-                <div className='content'>
-                    {/* {clients?.map((item) => {
-                        return (
-                            <div>
-                                <table>
-                                    <tr>
-                                        <td>aaaaa</td><td>{item.tipo == 'D' ? 'Deposito' : 'Saque'}</td><td>{item.data}</td>
-                                    </tr>
-                                </table>
-                            </div>
-                        )
-                    })} */}
-                    <table>
-                        <tr>
-                            <td>aa</td><td>bb</td>
-                        </tr>
-                    </table>
+  const dataSource = [
+    {
+      key: "1",
+      codigo:'1',
+      nome:'emanuele',
+      idade:'18',
+      altura: '1,67',
+      peso:'64,5',
+      nascimento:'20/04/2004',
+      acao: 'editar/ecluir',
+      ultimoPagamento: '16/02/2023'
 
-                </div>
-            </section>
-        </div>
-    )
-}
+    },
+  ];
+
+  const columns = [
+    {
+      title: "Código",
+      dataIndex: "codigo",
+      key: "codigo",
+    },
+    {
+      title: "Nome",
+      dataIndex: "nome",
+      key: "nome",
+    },
+    {
+      title: "Idade",
+      dataIndex: "idade",
+      key: "idade",
+    },
+    {
+      title: "Altura",
+      dataIndex: "altura",
+      key: "altura",
+    },
+    {
+      title: "Peso",
+      dataIndex: "peso",
+      key: "peso",
+    },
+    {
+      title: "Data de nascimento",
+      dataIndex: "nascimento",
+      key: "nascimento",
+    },
+    {
+      title: "Último pagamento",
+      dataIndex: "ultimoPagamento",
+      key: "ultimoPagamento",
+    },
+    {
+      title: "Ação",
+      dataIndex: "acao",
+      key: "acao",
+    },
+  ];
+  return (
+    <div>
+      <HeaderSecond titulo="Listar clientes" />
+      <section className="section">
+        <Table dataSource={dataSource} columns={columns} />
+      </section>
+    </div>
+  );
+};
 export default ListaClientes;
