381d1de13e1800f4db9c20653d8af3f6bcd59dad
Emanuele Maria <emanuuuelem@gmail.com>
2023-03-03 14:19:12 -0300

    finally

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,49 +1,95 @@
-import { Button, DatePicker, Form, Input, Modal } from "antd"
-import { useState } from 'react'
-import dayjs from 'dayjs'
-import customParseFormat from 'dayjs/plugin/customParseFormat';
-import './style.scss'
-import 'antd/dist/reset.css';
+import { Button, Form, Input, Modal } from "antd";
+import { useState } from "react";
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import "./style.scss";
+import "antd/dist/reset.css";
 import { useNavigate } from "react-router-dom";
 dayjs.extend(customParseFormat);
-const dateFormatList = 'DD/MM/YYYY';
+const dateFormatList = "DD/MM/YYYY";
 const Home = () => {
-    const navigate = useNavigate()
-    function linkTo(param) {
-        navigate(`/${param}`)
-    }
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const showModal = () => {
-        setIsModalOpen(true);
-    };
-    const handleOk = () => {
-        setIsModalOpen(false);
-    };
-    const handleCancel = () => {
-        setIsModalOpen(false);
-    };
-    return (
-        <>
-            <div className="body">
-                <Button style={{ margin: '12px', borderRadius: '500px', minWidth: '200px' }} onClick={() => linkTo('cadastro')} type="default" >Criar cliente</Button>
-                <Button style={{ margin: '12px', borderRadius: '500px', minWidth: '200px' }} onClick={() => linkTo('listarClientes')} type="default" >Listar clientes</Button>
-                <Button style={{ margin: '12px', borderRadius: '500px', minWidth: '200px' }} onClick={() => linkTo('listarPagamentos')} type="default" >Listar pagamentos</Button>
-                <Button style={{ margin: '12px', borderRadius: '500px', minWidth: '200px' }} onClick={() => showModal()} type="default" >Efetuar pagamento</Button>
-            </div>
-            <Modal title="Efetuar pagamento" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-                <Form className='form'>
-                    <Form.Item >
-                        <Input placeholder='Valor: ' type='number' />
-                    </Form.Item>
-                    <Form.Item>
-                        <DatePicker defaultValue={dayjs('01/01/2023', dateFormatList)} format={dateFormatList} />
-                    </Form.Item>
-                    <Form.Item>
-                        <Button style={{ borderRadius: '500px', minWidth: '200px', width: '100%' }} htmlType='submit' type='primary' onClick={() => linkTo('listarPagamentos')}>Efetuar pagamento</Button>
-                    </Form.Item>
-                </Form>
-            </Modal>
-        </>
-    )
-}
+  let data;
+  const navigate = useNavigate();
+  function linkTo(param) {
+    navigate(`/${param}`);
+  }
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [valor, setValor] = useState(null);
+  const [ultimoPagamento, setUltimoPagamento] = useState(data);
+
+  const [listaPagamentos, setListaPagamentos] = useState([]);
+
+  function efetuarPagamento() {
+    setListaPagamentos(() => {
+      const newLista = [...listaPagamentos];
+      newLista.unshift({ valor: Number(valor), ultimoPagamento: ultimoPagamento });
+      console.log(newLista);
+      return newLista;
+    });
+  }
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  return (
+    <>
+      <div className="body">
+        <Button
+          style={{ margin: "12px", borderRadius: "500px", minWidth: "200px" }}
+          onClick={() => linkTo("cadastro")}
+          type="default"
+        >
+          Criar cliente
+        </Button>
+        <Button
+          style={{ margin: "12px", borderRadius: "500px", minWidth: "200px" }}
+          onClick={() => linkTo("listarClientes")}
+          type="default"
+        >
+          Listar clientes
+        </Button>
+        <Button
+          style={{ margin: "12px", borderRadius: "500px", minWidth: "200px" }}
+          onClick={() => linkTo("listarPagamentos")}
+          type="default"
+        >
+          Listar pagamentos
+        </Button>
+        <Button
+          style={{ margin: "12px", borderRadius: "500px", minWidth: "200px" }}
+          onClick={() => showModal()}
+          type="default"
+        >
+          Efetuar pagamento
+        </Button>
+      </div>
+      <Modal
+        title="Efetuar pagamento"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <Input
+          placeholder="Valor: "
+          value={valor}
+          onChange={(e) => setValor(e.target.value)}
+          type="number"
+        />
+        <Button
+          onClick={() => efetuarPagamento(valor)}
+          style={{ borderRadius: "500px", minWidth: "200px", width: "100%" }}
+          htmlType="submit"
+          type="primary"
+        >
+          Efetuar pagamento
+        </Button>
+      </Modal>
+    </>
+  );
+};
 export default Home;
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 
 import Cadastro from './pages/cadastro/cadastro'
-import EfetuarPagamento from './pages/efetuarPagamento/efetuarPagamento'
 import Home from './pages/home/home'
 import ListaPagamentos from './pages/listaPagamentos/listaPagamentos'
 import ListaClientes from './pages/listaClientes/listaClientes'
@@ -11,10 +10,6 @@ export const router = createBrowserRouter([
     {
         path:"/cadastro",
         element: <Cadastro/>
-    },
-    {
-        path:"/efetuarPagamento",
-        element: <EfetuarPagamento/>
     },
     {
         path:"/",
