f5e5538ee98d0c4775fad6ab669777e7490fcd3b
Emanuele Maria <emanuuuelem@gmail.com>
2023-03-03 07:40:00 -0300

    feat: estados

--- src/pages/cadastro/cadastro.jsx
+++ src/pages/cadastro/cadastro.jsx
@@ -4,6 +4,7 @@ import dayjs from 'dayjs'
 import customParseFormat from 'dayjs/plugin/customParseFormat';
 import { useNavigate } from 'react-router-dom';
 import HeaderSecond from '../../components/HeaderSecond';
+import { useState } from 'react';
 dayjs.extend(customParseFormat);
 const dateFormatList = 'DD/MM/YYYY';
 
@@ -12,25 +13,52 @@ const Cadastro = () => {
     function linkTo(param) {
         navigate(`/${param}`)
     }
+    
+    let dia = new Date().getDate()
+    let mes = new Date().getMonth()+1
+    let ano = new Date().getFullYear()
+    let dataHoje
+    if(dia<10 && mes<10){
+         dataHoje = `0${dia}/0${mes}/${ano}`
+    } else if(dia<10 && mes>=10){
+         dataHoje = `0${dia}/${mes}/${ano}`
+    } else if(dia>=10 && mes<10){
+         dataHoje = `${dia}/${mes}/${ano}`
+    }
+    const [nome, setNome]=useState('')
+    const [idade, setIdade]=useState(null)
+    const [altura, setAltura]=useState(null)
+    const [peso, setPeso]=useState(null)
+    const [nascimento, setNascimento]=useState(dataHoje)
+    const [ultimoPagamento, setUltimoPagamento]=useState(dataHoje)
+    const cliente={
+        nome: nome,
+        idade: idade,
+        altura: altura,
+        nascimento: nascimento,
+        peso: peso,
+        ultimoPagamento: ultimoPagamento
+    }
+    console.log(cliente)
     return (
         <div>
             <HeaderSecond titulo= "Criar cliente"/>
             <section className='section-form'>
                 <Form className='form'>
                     <Form.Item >
-                        <Input placeholder='Nome completo: ' />
+                        <Input value={nome} onChange={e => setNome(e.target.nome)} placeholder='Nome completo: ' />
                     </Form.Item>
                     <Form.Item>
-                        <Input type='number' placeholder='Idade: ' />
+                        <Input value={idade} onChange={e => setIdade(e.target.idade)} type='number' placeholder='Idade: ' />
                     </Form.Item>
                     <Form.Item>
-                        <Input type='number' placeholder='Peso: ' />
+                        <Input value={peso} onChange={e => setPeso(e.target.peso)} type='number' placeholder='Peso: ' />
                     </Form.Item>
                     <Form.Item>
-                        <Input type='number' placeholder='Altura: ' />
+                        <Input value={altura} onChange={e => setAltura(e.target.altura)} type='number' placeholder='Altura: ' />
                     </Form.Item>
                     <Form.Item name="date-picker" label='Data de nascimento'>
-                        <DatePicker defaultValue={dayjs('01/01/2023', dateFormatList)} format={dateFormatList} />
+                        <DatePicker value={nascimento} onChange={e => setNascimento(e.target.nascimento)} defaultValue={dayjs(dataHoje, dateFormatList)} format={dateFormatList} />
                     </Form.Item>
                     <Form.Item>
                         <Button style={{ borderRadius: '500px', minWidth: '200px', width: '100%' }} htmlType='submit' type='primary' onClick={() => linkTo('listarClientes')}>Criar cliente</Button>
